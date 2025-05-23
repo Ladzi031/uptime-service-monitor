3e25153b4b7125340da8fba161a358ed77c89dff
ladzi031 <ladziladzani@gmail.com>
2025-05-23 18:29:55 +0200

    update README

--- src/main/java/za/ac/ladzani/uptimeMonitor/UptimeMonitorApplication.java
+++ src/main/java/za/ac/ladzani/uptimeMonitor/UptimeMonitorApplication.java
@@ -10,11 +10,3 @@ public class UptimeMonitorApplication {
 		SpringApplication.run(UptimeMonitorApplication.class, args);
 	}
 }
-/*
-	TODO
-	get uptime metric
-	set-up raspberry-PI and and document the process
-	push application and run
-	create a decent README file
-	finally celebrate
-* */
